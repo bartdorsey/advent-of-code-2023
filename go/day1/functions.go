2fcb565adf60592af4e0e7141364883fe26ee16f
Bart Dorsey <bart@bartdorsey.com>
2023-12-01 16:30:39 -0600

    GoLang: Day 1 Complete

--- go/day1/functions.go
+++ go/day1/functions.go
@@ -1,9 +1,116 @@
 package day1
 
-func Day1part1() string {
-	return "Hello"
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+func Day1part1(data string) int {
+	lines := strings.Split(data, "\n")
+	var sum int
+	var numbers []int
+	for _, line := range lines {
+		var first string
+		var last string
+		for _, letter := range line {
+			_, err := strconv.Atoi(string(letter))
+			if err == nil {
+				if first == "" {
+					first = string(letter)
+				}
+				last = string(letter)
+			}
+		}
+		num, err := strconv.Atoi(first + last)
+		if err != nil {
+			continue
+		}
+		numbers = append(numbers, num)
+	}
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
 
-func Day1part2() string {
-	return "Hello"
+func findWord(slice string) (string, error) {
+	words := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for _, word := range words {
+		if len(slice) < len(word) {
+			continue
+		}
+		found := true
+		for j := range word {
+			if word[j] != slice[j] {
+				found = false
+			}
+		}
+		if found == true {
+			return word, nil
+		}
+	}
+	return "", errors.New("word not found")
+}
+
+func string2num(str string) string {
+	switch {
+	case str == "one":
+		return "1"
+	case str == "two":
+		return "2"
+	case str == "three":
+		return "3"
+	case str == "four":
+		return "4"
+	case str == "five":
+		return "5"
+	case str == "six":
+		return "6"
+	case str == "seven":
+		return "7"
+	case str == "eight":
+		return "8"
+	case str == "nine":
+		return "9"
+	default:
+		return ""
+	}
+}
+
+func Day1part2(data string) int {
+	lines := strings.Split(data, "\n")
+	var sum int
+	var numbers []int
+	for _, line := range lines {
+		var first string
+		var last string
+		for i, letter := range line {
+			_, err := strconv.Atoi(string(letter))
+			if err == nil {
+				if first == "" {
+					first = string(letter)
+				}
+				last = string(letter)
+			} else {
+				slice := line[i:]
+				word, err := findWord(slice)
+				if err == nil {
+					if first == "" {
+						first = string2num(word)
+					}
+					last = string2num(word)
+				}
+			}
+		}
+		num, err := strconv.Atoi(first + last)
+		if err != nil {
+			continue
+		}
+		numbers = append(numbers, num)
+	}
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
--- go/main.go
+++ go/main.go
@@ -4,6 +4,8 @@ import (
 	"aoc/day1"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
@@ -12,8 +14,26 @@ func main() {
 	fmt.Println("Running Day:", *day)
 	switch *day {
 	case 1:
-		fmt.Println(day1.Day1part1())
-		fmt.Println(day1.Day1part2())
+		content, err := os.ReadFile("../data/day1.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Part 1")
+		fmt.Println(day1.Day1part1(`1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`))
+		fmt.Println(day1.Day1part1(string(content)))
+		fmt.Println("Part 2")
+		fmt.Println(day1.Day1part2(`two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`))
+		fmt.Println(day1.Day1part2(string(content)))
+
 	}
 
 }
