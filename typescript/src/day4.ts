f4dbee78507604ee6ef5a7f6c866636622a559f9
Bart Dorsey <bart@bartdorsey.com>
2023-12-05 11:38:03 -0600

    TypeScript: Day 4 complete

---
+++ typescript/src/day4.ts
@@ -0,0 +1,100 @@
+export function day4part1(data: string) {
+    const lines = data.trim().split('\n');
+    let total = 0;
+    for (const line of lines) {
+        const [_, winners_str, numbers_str] = line.split(/: | \| /);
+        const winners = winners_str
+            .split(/\s+/)
+            .map((w) => parseInt(w))
+            .filter((w) => !isNaN(w));
+        const numbers = numbers_str
+            .split(/\s+/)
+            .map((w) => parseInt(w))
+            .filter((w) => !isNaN(w));
+        let points = 0;
+        for (const n of numbers) {
+            if (winners.includes(n)) {
+                if (points === 0) {
+                    points = 1;
+                } else {
+                    points *= 2;
+                }
+            }
+        }
+        total += points;
+    }
+    return total;
+}
+
+interface CardCopies {
+    [card: string]: number;
+}
+
+// Memoize the card copies
+const card_copies: CardCopies = {};
+
+function processLine(card: string, line: string): number {
+    let num_copies = 0;
+    const [_, winners_str, numbers_str] = line.split(/: | \| /);
+    const winners = winners_str
+        .split(/\s+/)
+        .map((w) => parseInt(w))
+        .filter((w) => !isNaN(w));
+    const numbers = numbers_str
+        .split(/\s+/)
+        .map((w) => parseInt(w))
+        .filter((w) => !isNaN(w));
+    for (const n of numbers) {
+        if (winners.includes(n)) {
+            num_copies += 1;
+        }
+    }
+    card_copies[card] = num_copies;
+    return num_copies;
+}
+
+interface CardCounts {
+    [key: string]: number;
+}
+
+function processLines(
+    lines: string[],
+    num_copies = lines.length,
+    current_index = 0,
+    cards_counts: CardCounts = {}
+): CardCounts {
+    const from = current_index;
+    const to = num_copies + current_index;
+    for (let i = from; i < to; i++) {
+        const line = lines.at(i);
+        if (!line) {
+            continue;
+        }
+        const [card] = line.split(':');
+        // count the card
+        cards_counts[card] =
+            cards_counts[card] === undefined ? 1 : cards_counts[card] + 1;
+        const new_num_copies = card_copies[card]
+            ? card_copies[card]
+            : processLine(card, line);
+
+        current_index++;
+        cards_counts = processLines(
+            lines,
+            new_num_copies,
+            current_index,
+            cards_counts
+        );
+    }
+    return cards_counts;
+}
+
+export function day4part2(data: string) {
+    const lines = data.trim().split('\n');
+    const card_counts = processLines(lines);
+    let total = 0;
+    for (const card in card_counts) {
+        total += card_counts[card];
+    }
+    return total;
+}
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,6 +3,8 @@
 import { Command } from 'commander';
 import { day1part1, day1part2 } from './day1.js';
 import { day2part1, day2part2 } from './day2.js';
+import { day3part1, day3part2 } from './day3.js';
+import { day4part1, day4part2 } from './day4.js';
 import { readFile } from 'fs/promises';
 
 const program = new Command();
@@ -43,5 +45,37 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
         console.log('Part 2 = ', day2part2(part1test));
         console.log('Part 1 = ', day2part1(contents));
         console.log('Part 2 = ', day2part2(contents));
+    }
+    case '3': {
+        const contents = await readFile('../data/day3.txt', 'utf-8');
+        const part3test = `467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..`;
+        console.log('Running Day 3');
+        console.log('Part 1 = ', day3part1(part3test));
+        // console.log('Part 2 = ', day3part2(part3test));
+        // console.log('Part 1 = ', day3part1(contents));
+        // console.log('Part 2 = ', day3part2(contents));
+    }
+    case '4': {
+        const contents = await readFile('../data/day4.txt', 'utf-8');
+        const part4test = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
+        console.log('Running Day 4');
+        // console.log('Part 1 = ', day4part1(part4test));
+        // console.log('Part 1 = ', day4part1(contents));
+        console.log('Part 2 = ', day4part2(part4test));
+        console.log('Part 2 = ', day4part2(contents));
     }
 }
